c03475ae55dcaa0a396f2650dd1633d04713395c
Atul Kumar Awasthi <53579888+kumaratul60@users.noreply.github.com>
2021-08-09 08:21:49 +0530

    Delete todo.js

--- src/todo.js
+++
@@ -1,8 +0,0 @@
-// TODO: Add counter in addToBasket
-// TODO: Stripe webhooks db integration
-// TODO: enable online seach barcode
-// TODO: checkout intem add/re basket css part
-// TODO: Add some extra authentication
-// TODO: Home page product feed on hover product animation
-
-// stripe listen --forward-to localhost:3000/api/webhook
