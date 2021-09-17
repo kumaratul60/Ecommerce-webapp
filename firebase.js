15158610de998efdf74793cefb7a631ffb1a3a4b
Atul Kumar Awasthi <atulreso1@gmail.com>
2021-09-17 15:57:33 +0530

    update login

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCN3Yw14S15KVAgQIh8M0znTf-AFv4f3Cs",
-  authDomain: "amzn-2-1cc45.firebaseapp.com",
-  projectId: "amzn-2-1cc45",
-  storageBucket: "amzn-2-1cc45.appspot.com",
-  messagingSenderId: "476209874866",
-  appId: "1:476209874866:web:bc2eb30315d3ecab4746ab",
-  measurementId: "G-M5HJJ1DM4N",
+  apiKey: "AIzaSyCyMGTxr_UIEn68LPs0lpTQou5KYJpLBvk",
+  authDomain: "amzn-2-545d9.firebaseapp.com",
+  projectId: "amzn-2-545d9",
+  storageBucket: "amzn-2-545d9.appspot.com",
+  messagingSenderId: "834459139946",
+  appId: "1:834459139946:web:f2fddb3dcaff44cd256f13",
 };
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
